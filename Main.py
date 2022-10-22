c32bc859dd9574526605161828209aa46245bfae
Pedro Ribeiro Filho <prfilho@id.uff.br>
2022-10-21 22:58:52 -0300

    Feat : updated the main function, to be able to execute all the functions done previously

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from settings import *
+from level import Level
 
 class Game:
 	def __init__(self):
@@ -9,6 +10,8 @@ class Game:
 		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
 		pygame.display.set_caption('Chasing Death')
 		self.clock = pygame.time.Clock()
+
+		self.level = Level()
 	
 	def run(self):
 		while True:
@@ -16,11 +19,11 @@ class Game:
 				if event.type == pygame.QUIT:
 					pygame.quit()
 					sys.exit()
-
-			self.screen.fill('white')
+			self.screen.fill('black')
+			self.level.run()
 			pygame.display.update()
 			self.clock.tick(FPS)
 
 if __name__ == '__main__':
 	game = Game()
-	game.run()	
+	game.run()
