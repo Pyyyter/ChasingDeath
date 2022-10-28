7adf6407dd16c17a9f8296b8840911a1f0e10454
Pedro Ribeiro Filho <prfilho@id.uff.br>
2022-10-28 10:33:08 -0300

    Feat : Setup de level básico, feito com elementos de ambiente ( ainda bem simples ). Adicionei o .draw estático das pedras e do player. O desenho é feito percorrendo uma matriz aonde cada elemento define o que a função run (level.py) deve desenhar.

--- level.py
+++ level.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import *
-
+from tile import Tile
+from player import Player
 class Level:
     def __init__(self):
 
@@ -15,9 +16,15 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row in WORLD_MAP:
-            print(row)
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == "x":
+                    Tile((x,y),[self.visible_sprites])
 
+                if col == "p":
+                    Player((x,y),[self.visible_sprites])
     def run(self): 
         ##### Atualizar e desenhar o jogo #####
-        pass
+        self.visible_sprites.draw(self.display_surface)
--- player.py
+++ player.py
@@ -4,5 +4,5 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load("assets/mapaMundo1/player.png").convert_alpha()
+        self.image = pygame.image.load("assets/game/mapaMundo1/player.png").convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
