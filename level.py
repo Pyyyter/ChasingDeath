0144f42dcdf7342cebd7513f3845d8c480aa1615
Pedro Ribeiro Filho <prfilho@id.uff.br>
2022-10-28 14:54:52 -0300

    Feat : Adicionei movimentação na classe jogador, com normalização de vetor para não ter velocidade duplicada ao andar em diagonais

--- level.py
+++ level.py
@@ -2,29 +2,37 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
 class Level:
     def __init__(self):
 
-        ##### reconhecer a surface
+        # reconhecer a surface
         self.display_surface = pygame.display.get_surface()
 
-        ##### configurando grupos de sprites com propriedades diferentes #####
+        # configurando grupos de sprites com propriedades diferentes 
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
 
-        ##### configurando sprites
+        # configurando sprites
         self.create_map()
 
     def create_map(self):
         for row_index, row in enumerate(WORLD_MAP):
+
             for col_index, col in enumerate(row):
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
+
                 if col == "x":
-                    Tile((x,y),[self.visible_sprites])
+                    Tile((x,y),[self.visible_sprites, self.obstacles_sprites])
 
                 if col == "p":
-                    Player((x,y),[self.visible_sprites])
+                    self.player = Player((x,y),[self.visible_sprites])
+
+
     def run(self): 
         ##### Atualizar e desenhar o jogo #####
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+
+        debug(self.player.direction)
--- player.py
+++ player.py
@@ -6,3 +6,39 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load("assets/game/mapaMundo1/player.png").convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
+
+    def input(self):
+        # Reconhecendo entrada de teclado
+        keys = pygame.key.get_pressed()
+
+        # Definindo orientação no eixo Y com entradas do teclado
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+            
+        # Definindo orientação no eixo Y com entradas do teclado
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else :
+            self.direction.x = 0
+            
+    # Definindo função de movimento
+    def move(self,speed):
+        # Normalizando o vetor para não ter velocidade vetorial dobrada
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+
+        # movendo, de fato, a classe Player
+        self.rect.center += self.direction * speed
+
+    def update(self):
+        self.input()
+        self.move(self.speed)
