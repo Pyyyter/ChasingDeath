a2728233b48922cd6eff912e8059e1413566bf24
Pedro Ribeiro Filho <prfilho@id.uff.br>
2022-10-28 18:21:46 -0300

    * Feat : Adição da mecânica da câmera.

--- player.py
+++ player.py
@@ -2,12 +2,14 @@ import pygame
 from settings import *
 
 class Player(pygame.sprite.Sprite):
+
     def __init__(self, pos, groups, obstacles_sprites):
 
         # Definindo player
         super().__init__(groups)
         self.image = pygame.image.load("assets/game/mapaMundo1/player.png").convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+        self.hitbox = self.rect.inflate(0, -26)
 
 
         # Definindo vetores e velocidade
@@ -16,6 +18,8 @@ class Player(pygame.sprite.Sprite):
 
         # Definindo obstáculos
         self.obstacles_sprites = obstacles_sprites
+
+
 
     def input(self):
         # Reconhecendo entrada de teclado
@@ -37,48 +41,50 @@ class Player(pygame.sprite.Sprite):
         else :
             self.direction.x = 0
 
-    # Definindo função de movimento
+
+
     def move(self,speed):
+        # Definindo função de movimento
         # Normalizando o vetor para não ter velocidade vetorial dobrada
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
 
         # movendo, de fato, a classe Player
-        self.rect.x += self.direction.x * speed
+        self.hitbox.x += self.direction.x * speed
         self.collision('horizontal')
-        self.rect.y += self.direction.y * speed
+        self.hitbox.y += self.direction.y * speed
         self.collision('vertical')
+        self.rect.center = self.hitbox.center
 
 
-
-    # Função que detecta colisões
+    
     def collision(self, direction):
-
+        # Função que detecta colisões
 
         if direction == 'horizontal':
             for sprite in self.obstacles_sprites:
-                if sprite.rect.colliderect(self.rect):
+                if sprite.hitbox.colliderect(self.hitbox):
                     #movendo-se para a direita
                     if self.direction.x > 0:
-                        self.rect.right = sprite.rect.left
+                        self.hitbox.right = sprite.hitbox.left
 
                     #movendo-se para a esquerda
                     if self.direction.x < 0:
-                        self.rect.left = sprite.rect.right
+                        self.hitbox.left = sprite.hitbox.right
 
 
 
         if direction == 'vertical':
             for sprite in self.obstacles_sprites:
-                if sprite.rect.colliderect(self.rect):
+                if sprite.hitbox.colliderect(self.hitbox):
                     if self.direction.y > 0:
-                        self.rect.bottom = sprite.rect.top
+                        self.hitbox.bottom = sprite.hitbox.top
 
                     #movendo-se para cima
                     if self.direction.y < 0:
-                        self.rect.top = sprite.rect.bottom
+                        self.hitbox.top = sprite.hitbox.bottom
 
-    
+
 
     def update(self):
         self.input()
