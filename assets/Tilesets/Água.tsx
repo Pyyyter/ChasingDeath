e1526ed7d45297415cde4564a119b19f992a2ad9
Pyyyter <pyyyter@hotmail.com>
2022-12-15 16:40:04 -0300

    Feat : Adicionei o Mapa, e mecânica de colisão do mapa com arquivo CSV

---
+++ assets/Tilesets/Água.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Água" tilewidth="32" tileheight="32" tilecount="49" columns="7">
+ <image source="C:/Users/pyyyt/Downloads/e80a5eee359fbd3.png" width="255" height="255"/>
+</tileset>
--- level.py
+++ level.py
@@ -3,7 +3,7 @@ from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
-
+from support import import_csv_layout, import_folder
 class Level:
     def __init__(self):
 
@@ -18,17 +18,29 @@ class Level:
         self.create_map()
  
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
+        layouts = {
+            'borda': import_csv_layout('assets/Mapa/CSVs/Mapa_Floorblock.csv'),
+            'objetos': import_csv_layout('assets/Mapa/CSVs/Mapa_Objetos com colisão.csv'),
+            'plataformas': import_csv_layout('assets/Mapa/CSVs/Mapa_Plataformas.csv'),
+        }
+        # graphics = {
+            #  'grama': import_folder('')
+        # }
+        # overlay = pygame.image.load("assets/Mapa/Imagens/overlay.png").convert()
+        for style,layout in layouts.items():
+            for row_index, row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+                        if style == 'borda':
+                            Tile((x,y), [self.obstacles_sprites],'invisible')
+        #         if col == "x":
+        #             Tile((x,y),[self.visible_sprites, self.obstacles_sprites])
 
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-
-                if col == "x":
-                    Tile((x,y),[self.visible_sprites, self.obstacles_sprites])
-
-                if col == "p":
-                    self.player = Player((x,y),[self.visible_sprites], self.obstacles_sprites)
+        #         if col == "p":
+        #             self.player = Player((x,y),[self.visible_sprites], self.obstacles_sprites)
+        self.player  = Player((3000,2500),[self.visible_sprites],self.obstacles_sprites)
 
 
     def run(self): 
@@ -46,12 +58,31 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_width = self.display_surface.get_size()[0]//2
         self.half_height = self.display_surface.get_size()[1]//2
         self.offset = pygame.math.Vector2()
-    
+
+        # Criando overlay
+        self.overlay_surf = pygame.image.load("assets/Mapa/Imagens/overlay.png").convert_alpha()
+        self.overlay_rect = self.overlay_surf.get_rect(topleft = (0,0))
+
+
+        # Criando o chão
+        self.floor_surf = pygame.image.load("assets/Mapa/Imagens/Mapa.png").convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
+
     def custom_draw(self, player):
+
+        # desenhar as árvores
+        overlay_offset_x = self.overlay_rect.centerx-self.half_width
+        overlay_offset_y = self.overlay_rect.centery-self.half_height
+        self.display_surface.blit(self.floor_surf,(overlay_offset_x,overlay_offset_y))
 
         # recebendo o desvio do player
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        
+        # Desenhando o chão
+        floor_offset_pos = self.floor_rect.topleft-self.offset
+        self.display_surface.blit(self.floor_surf,floor_offset_pos)
 
 
         for sprite in sorted(self.sprites(), key = lambda sprite : sprite.rect.centery):
